e9ea15849bb1c7feb105e2b81ff376d160b2539d
Neo <71941576+NeoAIChan@users.noreply.github.com>
2022-02-24 17:08:19 +0300

    Beginning of Journey
    Index.js will be our main file, we mostly won't need to do anything with this file; but it is really important to make bot alive. That's why, we will be using command handler.

---
+++ index.js
@@ -0,0 +1,52 @@
+//Setup
+const aoijs = require('aoi.js');
+const config = require('./config.js');
+const bot = new aoijs.Bot(config.Bot);
+const fs = require('fs');
+
+const voice = new aoijs.Voice(bot, {
+  cache: {
+    cacheType: "Memory", 
+    enabled: true,
+  }, 
+});
+
+//Handlers
+require('./handlers/status')(bot);
+require('./handlers/variables')(bot); 
+require('./handlers/callbacks')(bot);
+
+//Events
+const files = fs.readdirSync('./events').filter(file => file.endsWith('js'))
+files.forEach( x => {
+	require(`./events/${x}`)(bot)
+});
+
+//Commands Loader & Colorful Console
+const loader = new aoijs.LoadCommands(bot)
+loader.load(bot.cmd, "./commands/")
+
+loader.setColors({
+  walking: ["blink", "dim", "fgWhite"],
+  failedWalking: {
+    name: ["bright", "fgYellow", "underline"],
+ 
+    text: ["gray", "fgRed"]
+  },
+  typeError: {
+    command: ["bright", "fgYellow"],
+    type: ["fgYellow"],
+    text: ["bright", "fgRed"]
+  },
+  failLoad: {
+    command: ["bright", "fgMagenta"],
+    type: ["fgRed"],
+    text: ["bright", "fgRed"],
+  },
+  loaded: {
+    command: ["bright", "fgRed"],
+    type: ["bright", "fgWhite"],
+    text: ["bright", "fgBlue"]
+  },
+ 
+});
