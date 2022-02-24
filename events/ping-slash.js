f03415a25e7099d541755706e6a11ac5f679dd47
Neo <71941576+NeoAIChan@users.noreply.github.com>
2022-02-24 21:55:37 +0300

    Delete ping-slash.js

--- events/ping-slash.js
+++
@@ -1,9 +0,0 @@
-module.exports = (bot) => {
-  bot.interactionCommand({
-    name: "ping", 
-    prototype: 'slash', 
-    code: `$interactionReply[$pingms for web socket, $dbPingms for database.]
-
-$onlyBotPerms[useappcmds;I'm missing \`USE_APPLICATION_COMMANDS\` permission.]`
-  })
-}
