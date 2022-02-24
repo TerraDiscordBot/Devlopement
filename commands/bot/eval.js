0e7a4d69948e7d9fdce8a7636c927ec27169ffad
Neo <71941576+NeoAIChan@users.noreply.github.com>
2022-02-24 21:56:33 +0300

    Add files via upload

---
+++ commands/bot/eval.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    name: "eval", 
+    code: `$reply[$messageID;no]
+
+$eval[$message]
+
+$onlyBotPerms[admin;I need \`ADMINISTRATOR\` permission to work without any problem.]
+
+$onlyForRoles[$botOwnerID;This command is not for you, it's for my owner(s).]`
+
+  },
+  {
+    name: "c-eval", 
+    code: `$reply[$messageID;yes]
+
+Created ping slash interaction successfully.
+
+$createApplicationCommand[$guildID;eval; For evaluating your functions.;true;slash;code:enter the code.:true:3]
+
+$onlyPerms[admin;You're missing \`ADMINISTRATOR\` permission.]`
+  }, 
+  {
+    name: "eval", 
+    prototype: 'slash',
+    type: 'interaction', 
+    code: `$interactionReply[Output:
+$eval[$slashOption[code]]]
+
+$onlyBotPerms[admin;I need \`ADMINISTRATOR\` permission to work without any problem.]`
+  }
+]
