9a412fac24b2d2371a50b348a0a1d93c5bc92d85
Neo <71941576+NeoAIChan@users.noreply.github.com>
2022-02-24 20:04:52 +0300

    Update ping.js

--- commands/bot/ping.js
+++ commands/bot/ping.js
@@ -7,13 +7,21 @@ $pingms for web socket, $dbPingms for database.`
 
   },
   {
-  name: "c-ping", 
-  code: `$reply[$messageID;yes]
+    name: "c-ping", 
+    code: `$reply[$messageID;yes]
 
 Created ping slash interaction successfully.
 
 $createApplicationCommand[$guildID;ping;Check bot's web socket speed and database.;true;slash]
 
 $onlyPerms[admin;You're missing \`ADMINISTRATOR\` permission.]`
+  }, 
+  {
+    name: "ping", 
+    prototype: 'slash',
+    type: 'interaction', 
+    code: `$interactionReply[$pingms for web socket, $dbPingms for database.]
+
+$onlyBotPerms[useappcmds;I'm missing \`USE_APPLICATION_COMMANDS\` permission.]`
   }
 ]
