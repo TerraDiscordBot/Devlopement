3e85b06b5e465f814b2f06e7f78f4ae99f4bd448
Neo <71941576+NeoAIChan@users.noreply.github.com>
2022-03-14 23:14:26 +0300

    hi

--- commands/general/afk.js
+++ commands/general/afk.js
@@ -1,58 +1,4 @@
 module.exports = [
-   {
-    name: "afk", 
-    prototype: 'slash',
-    type: 'interaction', 
-    code: `$changeNickname[$authorID;AFK︱$replaceText[$replaceText[$checkCondition[$nickname!=];true;$nickname];false;$username]]
-
-$onlyIf[$authorID!=$ownerID;{
-	"content": "I can't change your name cause you're owner. But, you're still on afk status with "$slashOption[reason]" reason.",
-	"options": {
-		"interaction": true
-		}
-	}
-]
-
-$deleteVar[afkStatus;$authorID]
-$deleteVar[afkReason;$authorID]
-
-$interactionReply[You're now afk with "$slashOption[reason]" reason.]
-    
-$onlyBotPerms[managenicknames;{
-	"content": "I need \`MANAGE_NICKNAMES\` permission.",
-	"options":{
-		"interaction" : true
-		}
-	}
-]`
-  },
-  {
-  	name: "$alwaysExecute",
-  	code: `$deleteIn[10s]
- 	
-$reply[$messageID;yes]
-
-The member you mentioned is afk, cause "$getUserVar[afkReason;$mentioned[1;no]]"
-
-$onlyIf[$getUserVar[afk;$mentioned[1;no]]>=1;]`
-
-  },
-  {
-  	name: "$alwaysExecute",
-  	code: `$deleteIn[10s]
-
-$deleteVar[afkStatus;$authorID]
-$deleteVar[afkReason;$authorID]
-
-$changeNickname[$authorID;$replaceText[$replaceText[$checkCondition[$nickname!=];true;$nickname];false;$username]]
-
-$reply[$messageID;yes]
-
-Welcome back, removed your afk reason.
-
-$onlyIf[$getUserVar[afkStatus;$authorID]>=1;]`
-
-  },
   {
     name: "c-afk", 
     code: `$reply[$messageID;yes]
@@ -62,5 +8,42 @@ Created afk slash interaction successfully.
 $createApplicationCommand[$guildID;afk;Set yourself on afk status.;true;slash;reason:please describe a reason.:true:3]
 
 $onlyPerms[admin;You're missing \`ADMINISTRATOR\` permission to create it.]`
-  }
+  }, 
+  {
+    name: "afk", 
+    prototype: 'slash',
+    type: 'interaction', 
+    code: `$interactionReply[You're now afk at "$parseDate[$dateStamp;date]" with "$slashOption[reason]" reason.]
+
+$setUserVar[afkStatus;yes]
+$setUserVar[afkReason;$slashOption[reason]]
+$setUserVar[afkOnTime;$dateStamp]`
+  },
+  {
+  	name: "$alwaysExecute",
+  	code: `$deleteIn[10s]
+ 	
+$reply[$messageID;yes]
+
+The member you mentioned is afk, cause "$getUserVar[afkReason;$mentioned[1;no]]" for $parseDate[$sub[$dateStamp;$getUserVar[afkonTime;$mentioned[1;no]]];time] 
+
+$onlyIf[$getUserVar[afkStatus;$mentioned[1;no]]==yes;]`
+
+  },
+  {
+  	name: "$alwaysExecute",
+  	code: `$deleteIn[10s]
+
+$deleteVar[afkStatus;$authorID]
+$deleteVar[afkReason;$authorID]
+$deleteVar[afkonTime;$authorID]
+
+$reply[$messageID;yes]
+
+Welcome back, you have been afk for $parseDate[$sub[$dateStamp;$getUservar[afkonTime]];time], reason: $getUserVar[afkReason]
+
+$onlyIf[$getUserVar[afk;$authorid]==yes;]`
+
+  },
+  
 ]
--- handlers/variables.js
+++ handlers/variables.js
@@ -2,6 +2,7 @@ module.exports = (bot) => {
 //afk variables
 	bot.variables({
 		afkReason: "no", 
-		afkStatus: "nothing."
+		afkStatus: "nothing.",
+		afkOnTime: "0"
 	})
 }
